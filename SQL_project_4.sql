01db437df3a29dbbf16965589874f6a2e542bb6d
Kevin Pohlmann <kevinpohlmann2201@gmail.com>
2021-12-17 12:11:54 +0100

    Add 4th SQL project: duplicate and even

---
+++ SQL_project_4.sql
@@ -0,0 +1,8 @@
+# Query a list of CITY names from STATION for cities that have an even ID number. Print the 
+# results in any order, but exclude duplicates from the answer.
+# The STATION table is described as follows:
+# CITY, ID, STATE
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE mod(ID,2) = 0;
